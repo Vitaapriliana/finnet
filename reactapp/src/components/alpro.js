37058c94e4a43e94c5dd712d25a3ad42944e4c86
akmalgb <akmal_gibran_28rpl@student.smktelkom-mlg.sch.id>
2021-09-16 09:19:13 +0700

    +add bootstrap table (uncompleted)

--- reactapp/src/components/alpro.js
+++ reactapp/src/components/alpro.js
@@ -7,6 +7,43 @@ class alpro extends React.Component {
         <div class="card-body">
           <h2>Alpro Critical</h2>
         </div>
+        <div class="container">
+          <div class="row g-0">
+            <div class="table-responsive">
+              <div class="table-responsive">
+                <table class="table table-bordered table-light border-dark">
+                  <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Lorem</th>
+                      <th scope="col">Ipsum</th>
+                      <th scope="col">Dolor</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td>Sit</td>
+                      <td>Amet</td>
+                      <td>Consectetur</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">2</th>
+                      <td>Adipisicing</td>
+                      <td>Elit</td>
+                      <td>Sint</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">3</th>
+                      <td colspan="2">Hic</td>
+                      <td>Temporibus</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
